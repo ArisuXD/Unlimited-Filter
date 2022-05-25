516232a25a03c8fa29f90bceb6351bf85554ccb0
ArisuXD <96303829+ArisuXD@users.noreply.github.com>
2022-05-25 15:56:04 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Discussion 💃", url="https://T.ME/MPMCHATZONE"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("film Channel", url="https://t.me/+GruRWNPEgXM2MjI1"),
+                    InlineKeyboardButton("Film Group", url="http://t.me/whatsappgang")
                 ]
             ]
         )
